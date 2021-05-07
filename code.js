a7df35d83142b1957e69cf65edde2cd4ec5abb8b
Chauncey Hester <c112h406@ku.edu>
2021-05-07 00:43:35 -0500

    Animation setup complete

--- code.js
+++ code.js
@@ -1,19 +1,25 @@
 var canvas;
 var ctx;
 
+var boardState = [];
+var activePieces = [];
+var fallingPieces = [];
+
+var turn = "p1"
+
 function load(){
+    for(var a = 0;a<7;a++){
+        boardState.push([]);
+        for(var b = 0; b<7;b++){
+            boardState[a].push([]);
+            boardState[a][b] = "empty"
+        }
+    }
     canvas = document.getElementById("GameView");
     ctx = canvas.getContext("2d");
     
-    ctx.fillStyle = "rgb(200,0,0)";
-
-    for(var x = 0; x < 7; x++){
-        for(var y = 0; y < 7; y++){
-            ctx.strokeRect(20+x*100,90+y*100,80,80);
-        }
-    }
-
     canvas.addEventListener("click",draw);
+    window.requestAnimationFrame(update);
 }
 
 function getMousePos(e){
@@ -46,7 +52,57 @@ function gridLookup(e){
 }
 
 
+function dropPieces(){
+
+}
+
+
+function update(){
+    
+
+    ctx.clearRect(0, 0, 720, 800);
+
+    
+
+    for(var x = 0; x < 7; x++){
+        for(var y = 0; y < 7; y++){
+            ctx.strokeRect(20+x*100,90+y*100,80,80);
+        }
+    }
+
+
+    for(var a = 0;a < activePieces.length;a++){
+        if(activePieces[a].player == "p1"){
+            ctx.fillStyle = "rgb(0,200,0)";
+        }
+        else{
+            ctx.fillStyle = "rgb(200,0,0)";
+        }
+        ctx.fillRect(20+activePieces[a].coordinate.x*100,90+activePieces[a].coordinate.y*100,80,80);
+    }
+
+    
+    window.requestAnimationFrame(update);
+
+}
+
+function switchPlayer(){
+    if(turn == "p1"){
+        turn = "p2"
+    }
+    else{
+        turn = "p1"
+    }
+
+}
+
+
 function draw(e){
     var coord = gridLookup(e);
-    ctx.fillRect(20+coord.x*100,90+coord.y*100,80,80)
+    if(boardState[coord.x][coord.y] != "filled"){
+        boardState[coord.x][coord.y] = "filled";
+        activePieces.push({player:turn,coordinate:coord});
+        switchPlayer();
+    }
+    
 }
